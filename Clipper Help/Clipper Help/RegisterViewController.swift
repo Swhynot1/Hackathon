609ebd7dbaf34d6406440bdc829ae42ce091f79a
JPChaus <48576328+JPChaus@users.noreply.github.com>
2019-03-16 11:00:36 -0400

    Login/register Screens

---
+++ Clipper Help/Clipper Help/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  Clipper Help
+//
+//  Created by student on 3/16/19.
+//  Copyright © 2019 Team_Yarmouth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class RegisterViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
