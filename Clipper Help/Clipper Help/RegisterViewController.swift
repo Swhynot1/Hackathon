607688a10322902672a46da1c4ba456e6688c3cd
JPChaus <48576328+JPChaus@users.noreply.github.com>
2019-03-16 15:05:53 -0400

    Add files via upload

--- Clipper Help/Clipper Help/RegisterViewController.swift
+++ Clipper Help/Clipper Help/RegisterViewController.swift
@@ -10,17 +10,9 @@ import Foundation
 import UIKit
 
 
+
 class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
-    @IBAction func StudentRegisterButton(_ sender: Any) {
-        performSegue(withIdentifier: "StudentSignUp", sender: self)
-    }
-    
-    
-    
-    
 }
