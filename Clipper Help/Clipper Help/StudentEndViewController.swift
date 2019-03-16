c63487bc3426e0061db31cb6ea2531d388526db3
JPChaus <48576328+JPChaus@users.noreply.github.com>
2019-03-16 14:42:47 -0400

    Add files via upload

---
+++ Clipper Help/Clipper Help/StudentEndViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StudentEndViewController.swift
+//  Clipper Help
+//
+//  Created by student on 3/16/19.
+//  Copyright © 2019 Team_Yarmouth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class StudentEndViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Clipper Help/Clipper Help/TeacherListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TeacherListViewController.swift
+//  Clipper Help
+//
+//  Created by student on 3/16/19.
+//  Copyright © 2019 Team_Yarmouth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class TeacherListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
